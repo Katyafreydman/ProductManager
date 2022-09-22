3ad7b152f7510c9046ca2874ba990353a0edd60f
Катя Фрейдман <katyafreydman@gmail.com>
2022-09-22 10:01:12 +0300

    Added files

---
+++ src/test/java/ProductManagerTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProductManagerTest {
+    ProductRepository repository = new ProductRepository();
+    ProductManager manager = new ProductManager(repository);
+    Product book = new Book(100, "Петровы в гриппе", 600, "Сальников А.");
+    Product smartphone = new Smartphone(200, "Samsung A71", 25000, "Samsung");
+    Product product = new Product(300, "Cake", 50);
+    @Test
+    void shouldAdd() {
+        manager.add(book);
+        Product[] expected = {book};
+        Product[] actual = repository.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldAddAll() {
+        manager.add(book);
+        manager.add(smartphone);
+        manager.add(product);
+
+        Product[] expected = { book, smartphone, product };
+        Product[] actual = repository.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldSearch() {
+        manager.add(book);
+        manager.add(smartphone);
+        manager.add(product);
+        String name = "Петровы в гриппе";
+
+        Product[] expected = {book};
+        Product[] actual = manager.searchBy(name);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+        @Test
+    void shouldSearchWhenProductsNotSuit() {
+        manager.add(book);
+        manager.add(smartphone);
+        manager.add(product);
+        String name = "Xiomi";
+
+        Product[] expected = {};
+        Product[] actual = manager.searchBy(name);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
---
+++ src/test/java/ProductRepositoryTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProductRepositoryTest {
+    private ProductRepository repository = new ProductRepository();
+    Product book = new Book(100, "Петровы в гриппе", 600, "Сальников А.");
+    Product smartphone = new Smartphone(200, "Samsung A71", 25000, "Samsung");
+    Product product = new Product(300, "Cake", 50);
+
+    @Test
+    public void saveItem() {
+        repository.save(book);
+
+        Product[] expected = {book};
+        Product[] actual = repository.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void removeById() {
+        repository.save(book);
+        repository.save(smartphone);
+        repository.save(product);
+        repository.removeById(100);
+
+        Product[] expected = { smartphone, product};
+        Product[] actual = repository.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void removeAllById() {
+        repository.save(book);
+        repository.save(smartphone);
+        repository.save(product);
+        repository.removeById(100);
+        repository.removeById(200);
+        repository.removeById(300);
+
+        Product[] expected = {};
+        Product[] actual = repository.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
+
+
+
