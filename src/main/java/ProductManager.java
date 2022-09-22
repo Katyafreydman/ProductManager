e19e9b621bbc25b382628cbe512f473edb4945f2
Катя Фрейдман <katyafreydman@gmail.com>
2022-09-22 10:12:03 +0300

    Added changes

--- src/main/java/ProductManager.java
+++ src/main/java/ProductManager.java
@@ -12,6 +12,7 @@ public class ProductManager {
             if (matches(product, text)) {
                 int length = result.length + 1;
                 Product[] tmp = new Product[length];
+                System.arraycopy(result, 0, tmp, 0, result.length);
                 int lastIndex = tmp.length - 1;
                 tmp[lastIndex] = product;
                 result = tmp;
