8f7baa5050e924e2550e4c8babb647752a441dfd
Катя Фрейдман <katyafreydman@gmail.com>
2022-09-26 14:21:41 +0300

    Added tests

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -5,12 +5,16 @@ public class ProductManagerTest {
     ProductRepository repository = new ProductRepository();
     ProductManager manager = new ProductManager(repository);
     Product book = new Book(100, "Петровы в гриппе", 600, "Сальников А.");
+
     Product smartphone = new Smartphone(200, "Samsung", 25000, "Samsung");
     Product product = new Product(300, "Cake", 50);
+    Product product2  = new Product(301, "Cake", 60);
+    Product smartphone2  = new Smartphone(201,"Samsung A71",30000,"Samsung");
 
     @Test
     void shouldAdd() {
         manager.add(book);
+
         Product[] expected = {book};
         Product[] actual = repository.findAll();
         Assertions.assertArrayEquals(expected, actual);
@@ -61,4 +65,44 @@ public class ProductManagerTest {
         Product[] actual = manager.searchBy(name);
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    void searchWithFewProduct() {
+        manager.add(book);
+        manager.add(smartphone);
+        manager.add(product);
+        manager.add(product2);
+
+        Product[] expected = {product, product2};
+        Product[] actual = manager.searchBy("Cake");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+@Test
+    void searchWithOneProduct () {
+
+    manager.add(book);
+    manager.add(smartphone);
+    manager.add(smartphone2);
+    manager.add(product);
+    manager.add(product2);
+
+    Product[] expected = {smartphone2};
+    Product[] actual = manager.searchBy("Samsung A71");
+    Assertions.assertArrayEquals(expected, actual);
+}
+
+    @Test
+    void searchWithAllProductNoFound () {
+
+        manager.add(book);
+        manager.add(smartphone);
+        manager.add(smartphone2);
+        manager.add(product);
+        manager.add(product2);
+
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Куприн");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
